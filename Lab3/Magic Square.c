ea8f54ab7fb55b74307d78023829439ce14dc1af
Mahmoud ElBanna <131378687+ElBanna532000@users.noreply.github.com>
2024-11-20 01:39:26 +0200

    Update and rename Magic Square to Magic Square.c

--- Lab3/Magic Square.c
+++ Lab3/Magic Square.c
@@ -2,16 +2,23 @@
 #include <stdlib.h>
 #include "conio.h"
 
-
 int main()
 {
     int size;
-    printf("Enter size of Matrix: ");
-    scanf("%d",&size);
-
+    int flag=1;
+    while(flag){
+        printf("Enter size of Matrix: ");
+        if(scanf("%d",&size)!=1 || size%2==0 || size<=0){
+            printf("Not a valid input, please try again (only odd postive integers are allowed)\n");
+            while (getchar() != '\n');
+        }else
+            flag=0;
+        }
+    system("clear");
+    printf("Matrix of size %dx%d", size,size);
     int row=1, col=(size+1)/2, num;
     for(num=1; num<=size*size; num++){
-        gotoxy(col*4,row*3);                
+        gotoxy(col*4,row*3);
         sleep(2);
         printf("%d", num);
 
@@ -30,8 +37,5 @@ int main()
                 col=size;
         }
     }
-
-
-
     return 0;
 }
